c929e9d6e5f882d290856cff1a2a680d0e1dfea2
AKJAW <akjaworski1@gmail.com>
2023-10-23 20:33:25 +0200

    chore: break formatting

--- androidApp/src/androidMain/kotlin/com/myapplication/MainActivity.kt
+++ androidApp/src/androidMain/kotlin/com/myapplication/MainActivity.kt
@@ -5,7 +5,7 @@ import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
